55926b2daac79ea05b7336cb9d64bb5b42909880
moonChen2 <bxwow@icloud.com>
2025-11-20 13:31:33 +0800

    注释，const 左值引用可以接受右值

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -15,6 +15,9 @@ int main(){
 
     for(int i = 0;i<100;i++){
         for(int j = 0;j<200;j++){
+            //这里传入的color是一个右值
+            //set pixel中的color参数如果是左值引用就会报错
+            //左值常引用可以接受右值
             film.setPixel(j,i,{0.5,0.7,0.2});
         }
     }
